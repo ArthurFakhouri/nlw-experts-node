f9d57840b4e96cb438cb8eac6f2489f5f91c5245
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-02-10 19:52:52 -0300

    config: Setting up websocket in our application

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { routes } from '../routes'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 
 const app = fastify()
 
+app.register(websocket)
 app.register(cookie, {
     secret: "polls-app-nlw", // for cookies signature
     hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
